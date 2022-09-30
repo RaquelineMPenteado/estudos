1af3356dbb302e33505b916874e0bdc93d76f20b
RaquelineMPenteado <raqueline.penteado@gmail.com>
2022-09-30 06:44:14 -0300

    correcoes

--- .vscode/conta_bancaria_main.rb
+++ .vscode/conta_bancaria_main.rb
@@ -3,8 +3,13 @@
 require "./conta_bancaria.rb"
 require "./conta_bancaria_tarifa.rb"
 
-conta1 = ContaBancariaTarifa.new(500)
-conta2 = ContaBancariaTarifa.new(100)
-conta1.transfere(conta2, 100)
-p "Novo saldo conta1 #{conta1.saldo}"
-p "Novo saldo conta1 #{conta2.saldo}"
+begin
+    conta1 = ContaBancariaTarifa.new(500)
+    conta2 = ContaBancariaTarifa.new(100)
+    conta1.transferir(conta2, 100)
+    p "Novo saldo conta1 #{conta1.saldo}"
+    p "Novo saldo conta1 #{conta2.saldo}"        
+    #conta1.transferir(conta2, 1000)       
+rescue StandardError => meu_erro
+    p "Não foi possível tranferir: #{meu_erro.message}"    
+end
